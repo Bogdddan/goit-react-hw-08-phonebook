cadf778cfc04503dffbe565eecefa09a1611d8d7
Bogdddan <bogdankrajnik95@gmail.com>
2023-07-13 15:30:32 +0300

    useAuth setting

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,14 +1,15 @@
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 import {
     selectUser,
     selectIsLoggedIn,
 } from './../redux/auth/selectors';
 
 export const useAuth = () => {
-    const isLogedIn = useSelector(selectIsLoggedIn);
+    const isLoggedIn = useSelector(selectIsLoggedIn);
     const user = useSelector(selectUser);
+    
     return {
-        isLogedIn,
-        user,
+      isLoggedIn,
+      user,
     };
 };
