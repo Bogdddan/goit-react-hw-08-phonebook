537e381a188b8222229b251089fab67319cb20ad
Bogdddan <bogdankrajnik95@gmail.com>
2023-07-13 13:52:30 +0300

    add operations logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
