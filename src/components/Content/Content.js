b1e3cf2701e3afa515401f7e1ada1b309bc729f5
Bogdddan <bogdankrajnik95@gmail.com>
2023-07-13 15:41:08 +0300

    register logic

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,14 @@
-import { useAuth } from "hooks";
+import { useAuth } from '../../hooks/index';
 
 function Content() {
-  const {
-    user: { name, email }
-  } = useAuth();
+    const {
+        user: { name, email }
+    } = useAuth();
+
     return <div>
-      <div>Content component</div>
-      <div>your name is {name}, email {email}</div>
+        <div>I am Content component.</div>
+        <div>You name is: {name}</div>
+        <div>You email is: {email}</div>
     </div>
 }
 
