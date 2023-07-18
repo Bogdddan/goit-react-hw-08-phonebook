e399666455b518668aff98685931ea1fd4952057
Bogdddan <bogdankrajnik95@gmail.com>
2023-07-18 23:59:30 +0300

    corect exit code 128 ❌

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -71,31 +71,3 @@ function Content() {
 }
 
 export default Content;
-
-
-// працюючий код 
-// import { useAuth } from '../../hooks/index';
-// import { useDispatch } from 'react-redux';
-// import { logOut } from '../../redux/auth/operations';
-
-// function Content() {
-//     const {
-//         user: { name, email }
-//     } = useAuth();
-
-//     const dispatch = useDispatch();
-//     const handleLogout = () => {
-//         dispatch(logOut());
-//     }
-
-//     return <div>
-//         <div>I am Content component.</div>
-//         <div>You name is: {name}</div>
-//         <div>You email is: {email}</div>
-//         <div>
-//             <button onClick={handleLogout}>Log out</button>
-//         </div>
-//     </div>
-// }
-
-// export default Content;
